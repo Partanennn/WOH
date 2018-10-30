8b5e1d4f8d605e9fbf3935a2b6b6344dd84cd0a3
Partanennn <44446649+Partanennn@users.noreply.github.com>
2018-10-30 23:08:51 +0200

    Add files via upload

---
+++ Node.js/controller.js
@@ -0,0 +1,55 @@
+'use strict'
+
+var mysql = require('mysql');
+
+// Connect to database
+const CONNECTION = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'harkka'
+});
+
+module.exports = 
+{
+    // Fetch all data from users table "/users/all"
+    fetchAll: (req, res) => {
+        CONNECTION.query('SELECT * FROM users', (err, result, fields) => {
+            if(err) {
+                console.log("Error while trying to get all data from users-table, reason: " + err);
+                res.status(500).json({'status': 'not ok', 'status_text: ': err.sqlMessage });
+            } else {
+                console.log("Succesfully fetched all data from users table, clock: "+time());
+                res.status(200).json(result);
+            }
+        });
+    },
+    // Fetch user and password from user table "/users/:tunnus/:salasana"
+    fetchOneUser: (req, res, next) => {
+        var username = req.params.tunnus;
+        var password = req.params.salasana;
+        CONNECTION.query('SELECT tunnus, salasana FROM users WHERE tunnus=?, salasana=?', [username, salasana],
+            (error, result, fields) => {
+                if(error) {
+                    console.log("Error while fetching user and password from user table, reason: " + error);
+                    res.json({"status": 500, "error": error, "response": null});
+                } else {
+                    console.log("Succesfully fetched user and password from user table, "+time());
+                    res.status(200).json(result);
+                }
+            }
+        );
+    }
+}
+
+// Sets time when data have been fetch from server or set to database
+function time() {
+    var current = new Date();
+    if(current.getMinutes() > 9 && current.getSeconds() > 9)
+        return current.getDate()+"."+(current.getMonth()+1)+"."+current.getFullYear()+" "+current.getHours()+":"+current.getMinutes()+"."+current.getSeconds();
+    else if(current.getMinutes() < 10 && current.getSeconds() > 9)
+        return current.getDate()+"."+(current.getMonth()+1)+"."+current.getFullYear()+" "+current.getHours()+":0"+current.getMinutes()+"."+current.getSeconds();
+    else if(current.getMinutes() > 9 && current.getSeconds() < 10)
+        return current.getDate()+"."+(current.getMonth()+1)+"."+current.getFullYear()+" "+current.getHours()+":"+current.getMinutes()+".0"+current.getSeconds();
+
+}

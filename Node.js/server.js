bf098ba334ff9d1cbbb17551499d37f286c6816f
partanennn <aleksi.partanenn17@gmail.com>
2018-10-31 10:53:56 +0200

    server update1

--- Node.js/server.js
+++ Node.js/server.js
@@ -1,2 +1,21 @@
 var express = require('express');
-var controller = require('controller.js');
+var app = express();
+var controller = require('./controller');
+var port = 3001;
+
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+app.use(allowCrossDomain);
+
+
+
+app.listen(port, () => {
+    console.log("Server is running at port "+port);
+});

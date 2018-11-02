4f80d952e1d9d016598c4dc52e509e0c97d93760
partanennn <aleksi.partanenn17@gmail.com>
2018-11-02 13:28:22 +0200

    Register jquery start

--- JQuery/Register.js
+++ JQuery/Register.js
@@ -1,5 +1,11 @@
 $(() => {
     $("#reg_button").click(() => {
-        var username = $("#").val();
+        var name = $("#name_reg").val();
+        var username = $("#username_reg").val();
+        var pass = $("#password_reg").val();
+        var address = $("#address_reg").val();
+        var city = $("#city_reg").val();
+        var role = $("#reg_select").val();  // 0=Yksityinen, 1=yritys
+
     });
 });

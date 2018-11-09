97829432966486b6c01f3a1b63f9844e1237beb9
Partanennn <aleksi.partanenn17@gmail.com>
2018-11-09 23:53:05 +0200

    Updating workorders table

--- SQL/harkka.sql
+++ SQL/harkka.sql
@@ -23,16 +23,21 @@ CREATE TABLE harkka.workorders
 (
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     order_username varChar(10) NOT NULL,
-    address varChar(50) NOT NULL,
-    city varChar(20) NOT NULL,
-    start_time DATE NOT NULL,
-    end_time DATE NOT NULL,
-    description varChar(200),
-    status varChar(20),
-    price DECIMAL(7, 2), /*Jos saa arvon 0,01, niin se on tarjouspyyntö*/
+    work_description varChar(200) NOT NULL,
+    orderdate DATETIME NOT NULL DEFAULT now(),
+    startdate DATETIME,
+    readydate DATETIME,
+    accepteddate DATETIME,
+    denieddate DATETIME,
+    comment_of_work varChar(200),
+    hours int,
+    comment_of_used_material varChar(200),
+    approx_budget int,
     CONSTRAINT workorders_username_fk FOREIGN KEY (order_username) REFERENCES users(username)
 )
 ENGINE = InnoDB;
+
+
 
 INSERT INTO states (status) VALUES("TILATTU");
 INSERT INTO states (status) VALUES("ALOITETTU");
@@ -47,3 +52,5 @@ INSERT INTO users(username, password, name, visiting_address) VALUES('Aleksi1', 
 INSERT INTO users(username, password, name, visiting_address) VALUES('moi123', 'moi1', 'Jonne Jokunen', 'Mannerheiminkatu 23');
 INSERT INTO users(username, password, name, visiting_address) VALUES('Super23456', 'SuperPassword123', 'Jorma Penttinen', 'Pohjolankatu 43');
 INSERT INTO users(username, password, name, visiting_address, role) VALUES('ISS', '1234', 'ISS Oy', 'Kaivotie 23', 'corporation');
+
+INSERT INTO workorders(order_username, description, orderdate) VALUES("Aleksi1", "Tää on testi", CURRENTDATE());

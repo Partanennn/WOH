9ac451c2911a1c0ead66a6fb2a6bdd090c6693dc
Partanennn <aleksi.partanenn17@gmail.com>
2018-11-14 21:37:04 +0200

    Added username and name for settings page

--- JQuery/Settings.js
+++ JQuery/Settings.js
@@ -2,7 +2,8 @@ $(() => {
     
     $.get("http://localhost:3001/users/"+sessionStorage['login_username'])
     .done((data, status, jqXHR) => {
-        
+        $("#name_reg").val(data[0].name);
+        $("#username_reg").val(data[0].username);
     });
     
     $.get("http://localhost:3001/housing_types")

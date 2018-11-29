fda4ece17a0d616e3b65d21ff570383c66c4d4bb
Partanennn <aleksi.partanenn17@gmail.com>
2018-11-29 11:17:50 +0200

    Removed some files

--- JQuery/offer.js
+++ JQuery/offer.js
@@ -2,89 +2,192 @@ $(() => {
     // Makes table full workorders with status JÄTETTY, VASTATTU, HYVÄKSYTTY, HYLÄTTY,
     // if user doesn't have any orders with those statuses, 
     // then writes EI TARJOUSPYYNTÖJÄ on screen
-    $.get(
-        "http://localhost:3001/workOrders/"+
-        sessionStorage['login_username']
-    )
-    .done((data, status, jqXHR) => {
-        var emptyTable = true;
-        $.each(data, (i, data) => {
-            var button = "";
-            if(data.status == "JÄTETTY" || data.status == "VASTATTU" || data.status == "HYVÄKSYTTY" || data.status == "HYLÄTTY") {
-                if(emptyTable) {
+    if(sessionStorage['login_role'] == "user") {
+        $.get(
+            "http://localhost:3001/workOrders/"+
+            sessionStorage['login_username']
+        )
+        .done((data, status, jqXHR) => {
+            var emptyTable = true;
+            $.each(data, (i, data) => {
+                var button = "";
+                if(data.status == "JÄTETTY" || data.status == "VASTATTU" || data.status == "HYVÄKSYTTY" || data.status == "HYLÄTTY") {
+                    if(emptyTable) {
+                        $("#offers_table").append(
+                            "<thead>" + 
+                            "<th>Tilaaja</th>" +
+                            "<th>Työ info</th>" +
+                            "<th>Katuosoite</th>" +
+                            "<th>Kaupunki</th>" +
+                            "<th>Tilauspäivä</th>" +
+                            "<th>Aloituspäivä</th>" +
+                            "<th>Valmistumispäivä</th>" +
+                            "<th>Kommentti tehdystä työstä</th>" +
+                            "<th>Käytetty tuntimäärä</th>" +
+                            "<th>Kustannusarvio</th>" +
+                            "<th>Tarvikkeet</th>" +
+                            "<th>Status</th>" +
+                            "</thead>" +
+                            "<tbody>"
+                        );
+                    }
+                    emptyTable = false;
+    
+                    if(data.status == "JÄTETTY") {
+                        button = "<td><button class='edit_button' data-editid="+ data.order_id +">Muokkaa</button></td><td><button class='delete' data-deleteid="+ data.order_id +">Poista</button></td>";
+                    }
+    
                     $("#offers_table").append(
-                        "<thead>" + 
-                        "<th>Tilaaja</th>" +
-                        "<th>Työ info</th>" +
-                        "<th>Katuosoite</th>" +
-                        "<th>Kaupunki</th>" +
-                        "<th>Tilauspäivä</th>" +
-                        "<th>Aloituspäivä</th>" +
-                        "<th>Valmistumispäivä</th>" +
-                        "<th>Kommentti tehdystä työstä</th>" +
-                        "<th>Käytetty tuntimäärä</th>" +
-                        "<th>Kustannusarvio</th>" +
-                        "<th>Tarvikkeet</th>" +
-                        "<th>Status</th>" +
-                        "</thead>" +
-                        "<tbody>"
+                        "<tr>" +
+                        "<td>" + data.order_username + "</td>"+
+                        "<td>" + data.work_description + "</td>" +
+                        "<td>" + data.address + "</td>" +
+                        "<td>" + data.city + "</td>" +
+                        "<td>" + dateFormatter(data.orderdate) + "</td>" +
+                        "<td>" + dateFormatter(data.startdate) + "</td>" +
+                        "<td>" + dateFormatter(data.readydate) + "</td>" +
+                        "<td>" + data.comment_of_work + "</td>" +
+                        "<td>" + data.hours + "</td>" +
+                        "<td>" + data.approx_budget + "</td>" +
+                        "<td>" + data.comment_of_used_material + "</td>" +
+                        "<td>" + data.status + "</td>" +
+                        button + 
+                        "</tr>"
                     );
+                    // Closes table body
+                    $("#offers_table").append("</tbody>");
+                        
+                    // Calls deleteWorkorder and pass order_id to function, this is added to every delete button
+                    $(".delete").click(function(){
+                        var id = $(this).attr("data-deleteid");
+                        deleteWorkorder(id);
+                    });
+    
+                    // Opens dialog and calls editWorkorder and pass order_id to function to get info of workorder,
+                    // Is added to every edit button
+                    $(".edit_button").click(function() {
+                        $("#edit_dialog").dialog("open");
+                        var id = $(this).attr('data-editid');
+                        workorder_id = id;
+                        editWorkorder(id);
+                    }); 
                 }
-                emptyTable = false;
-
-                if(data.status == "JÄTETTY") {
-                    button = "<td><button class='edit_button' data-editid="+ data.order_id +">Muokkaa</button></td><td><button class='delete' data-deleteid="+ data.order_id +">Poista</button></td>";
-                }
-
-                $("#offers_table").append(
-                    "<tr>" +
-                    "<td>" + data.order_username + "</td>"+
-                    "<td>" + data.work_description + "</td>" +
-                    "<td>" + data.address + "</td>" +
-                    "<td>" + data.city + "</td>" +
-                    "<td>" + dateFormatter(data.orderdate) + "</td>" +
-                    "<td>" + dateFormatter(data.startdate) + "</td>" +
-                    "<td>" + dateFormatter(data.readydate) + "</td>" +
-                    "<td>" + data.comment_of_work + "</td>" +
-                    "<td>" + data.hours + "</td>" +
-                    "<td>" + data.approx_budget + "</td>" +
-                    "<td>" + data.comment_of_used_material + "</td>" +
-                    "<td>" + data.status + "</td>" +
-                    button + 
-                    "</tr>"
-                );
-                // Closes table body
-                $("#offers_table").append("</tbody>");
-                    
-                // Calls deleteWorkorder and pass order_id to function, this is added to every delete button
-                $(".delete").click(function(){
-                    var id = $(this).attr("data-deleteid");
-                    deleteWorkorder(id);
-                });
-
-                // Opens dialog and calls editWorkorder and pass order_id to function to get info of workorder,
-                // Is added to every edit button
-                $(".edit_button").click(function() {
-                    $("#edit_dialog").dialog("open");
-                    var id = $(this).attr('data-editid');
-                    workorder_id = id;
-                    editWorkorder(id);
-                }); 
+            });
+    
+            // Checks if user haves any offers
+            if(emptyTable) {
+                $("#section").append("<h1>Ei tarjouspyyntöjä</h1>");
             }
-        });
-
-        // Checks if user haves any offers
-        if(emptyTable) {
-            $("#section").append("<h1>Ei tarjouspyyntöjä</h1>");
         }
+        ).fail( (jqXHR, status, err) => {
+            console.log("Status=" + status + ", " + err);
+        });
+    } else {
+        $.get(
+            "http://localhost:3001/workOrders/"
+        )
+        .done((data, status, jqXHR) => {
+            var emptyTable = true;
+            $.each(data, (i, data) => {
+                var button = "";
+                if(data.status == "JÄTETTY" || data.status == "VASTATTU" || data.status == "HYVÄKSYTTY" || data.status == "HYLÄTTY") {
+                    if(emptyTable) {
+                        $("#offers_table").append(
+                            "<thead>" + 
+                            "<th>Tilaaja</th>" +
+                            "<th>Työ info</th>" +
+                            "<th>Katuosoite</th>" +
+                            "<th>Kaupunki</th>" +
+                            "<th>Tilauspäivä</th>" +
+                            "<th>Aloituspäivä</th>" +
+                            "<th>Valmistumispäivä</th>" +
+                            "<th>Kommentti tehdystä työstä</th>" +
+                            "<th>Käytetty tuntimäärä</th>" +
+                            "<th>Kustannusarvio</th>" +
+                            "<th>Tarvikkeet</th>" +
+                            "<th>Status</th>" +
+                            "</thead>" +
+                            "<tbody>"
+                        );
+                    }
+                    emptyTable = false;
+    
+                    if(data.status == "JÄTETTY") {
+                        button = "<td><button class='edit_button' data-editid="+ data.order_id +">Vastaa</button></td><td><button class='delete' data-deleteid="+ data.order_id +">Poista</button></td>";
+                    }
+    
+                    $("#offers_table").append(
+                        "<tr>" +
+                        "<td>" + data.order_username + "</td>"+
+                        "<td>" + data.work_description + "</td>" +
+                        "<td>" + data.address + "</td>" +
+                        "<td>" + data.city + "</td>" +
+                        "<td>" + dateFormatter(data.orderdate) + "</td>" +
+                        "<td>" + dateFormatter(data.startdate) + "</td>" +
+                        "<td>" + dateFormatter(data.readydate) + "</td>" +
+                        "<td>" + data.comment_of_work + "</td>" +
+                        "<td>" + data.hours + "</td>" +
+                        "<td>" + data.approx_budget + "</td>" +
+                        "<td>" + data.comment_of_used_material + "</td>" +
+                        "<td>" + data.status + "</td>" +
+                        button + 
+                        "</tr>"
+                    );
+                    // Closes table body
+                    $("#offers_table").append("</tbody>");
+                        
+                    // Calls deleteWorkorder and pass order_id to function, this is added to every delete button
+                    $(".delete").click(function(){
+                        var id = $(this).attr("data-deleteid");
+                        deleteWorkorder(id);
+                    });
+    
+                    // Opens dialog and calls editWorkorder and pass order_id to function to get info of workorder,
+                    // Is added to every edit button
+                    $(".edit_button").click(function() {
+                        $("#admin_edit_dialog").dialog("open");
+                        var id = $(this).attr('data-editid');
+                        workorder_id = id;
+                        admineditWorkorder(id);
+                    }); 
+                }
+            });
+    
+            // Checks if user haves any offers
+            if(emptyTable) {
+                $("#section").append("<h1>Ei tarjouspyyntöjä</h1>");
+            }
+        }
+        ).fail( (jqXHR, status, err) => {
+            console.log("Status=" + status + ", " + err);
+        });
     }
-    ).fail( (jqXHR, status, err) => {
-        console.log("Status=" + status + ", " + err);
+
+
+
+    $("#admin_edit_dialog").dialog({
+        autoOpen: false,
+        buttons: [
+            {
+                text: "Hyväksy",
+                click: () => {
+
+                }
+            },
+            {
+                text: "Hylkää",
+                click: () => {
+
+                }
+            },
+            {
+                text: "Peruuta",
+                click: function() {
+                    $(this).dialog("close");
+                }
+            }
+        ]
     });
-
-
-
-
 
     $("#edit_dialog").dialog({
         autoOpen: false,
@@ -128,6 +231,7 @@ $(() => {
     $("#add_button").click(() => {
         $("#add_dialog").dialog("open");
     });
+
 
 
 
@@ -201,6 +305,18 @@ $(() => {
             $("#edit_city").val( data[0].city );
         });
 
+    }
+
+    function admineditWorkorder(id) {
+        $.get(
+            "http://localhost:3001/workorder/"+
+            id
+        )
+        .done( (data, status, jqXHR) => {
+            $("#admin_edit_work_desc").val( data[0].work_description );
+            $("#admin_edit_address").val( data[0].address );
+            $("#admin_edit_city").val( data[0].city);
+        })
     }
 
     // Checks if edit workorder textboxes are empty, if not then updates workorder and "refresh" page
